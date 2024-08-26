510618b4b9ef46ed656cd25bacd0143a9e9c9b7b
gerry <gerrydesrian488@gmail.com>
2024-08-26 13:53:16 +0700

    form display func

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+ 
+  const showForm = ref(true)
+
+  const toggleDisplay = () => {
+    showForm.value = !showForm.value
+  }
+
+</script>
+
 <template>
   <main>
     <div class="container">
       <header>
         <h1 class="header-title">Memo</h1>
-        <button class="header-button">+</button>
+        <button class="header-button" @click="toggleDisplay">+</button>
       </header>
       <div class="card-container">
         <div class="card">
         <p class="card-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit architecto tenetur corporis odit beatae voluptatibus labore totam sapiente quisquam iusto hic minima consequatur culpa vel quis molestias, doloremque veritatis fugit?</p>
         <p class="card-date">12/12/2024</p>
       </div>
+      </div>
+    </div>
+    <div class="form-overlay" v-if="showForm">
+      <div class="form-modal">
+        <button class="form-close-btn" @click="toggleDisplay">&times;</button>
+        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button class="form-save-btn" @click="toggleDisplay">save</button>
       </div>
     </div>
   </main>
@@ -65,5 +83,52 @@ header {
   margin-bottom: 20px;
   display: flex;
   flex-direction: column;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: gray;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 15px;
+  color: white;
+}
+
+.form-close-btn {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
 }
 </style>
